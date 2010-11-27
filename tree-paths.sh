6db8c1e861e54c0e0d70a384c3c7e724bde6fe71
Here I. Come <me.detected@gmail.com>
2010-11-27 03:14:40 +0300

    source files for writing tree and paths variables

---
+++ tree-paths.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# default file names
+
+TREEFILENAME="./tree"
+HASHTREEFILENAME="./sha512"
---
+++ tree-write.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# exit on any error
+set -e
+
+which find
+which gpg
+which xargs
+which sha512sum
+
+source "$(which tree-paths.sh)"
+
+# generate tree
+if [ -a "${TREEFILENAME}" ]; then
+  rm -f "${TREEFILENAME}"
+fi
+echo generating tree...
+find . -type f | sort > "${TREEFILENAME}"
+# sign tree. The signing happens w/ the default key. You are asked about your
+# password
+echo signing tree...
+gpg -a -s "${TREEFILENAME}"
+# make hashes of files
+if [ -a "${HASHTREEFILENAME}" ]; then
+  rm -f "${HASHTREEFILENAME}"
+fi
+echo writing file hash sums...
+find . -type f -print0 | xargs -0 -L1 sha512sum > "${HASHTREEFILENAME}"
+# sign hashes
+gpg -a -s "${HASHTREEFILENAME}"
