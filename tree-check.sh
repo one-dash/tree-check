3c08c5ccbeb5a22d44076c59e0bd1ba43d51c2c4
Here I. Come <me.detected@gmail.com>
2010-11-27 04:24:53 +0300

    tree ckecking code

---
+++ tree-check.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+source $(which tree-paths.sh)
+
+which diff
+
+# check hashes signature
+gpg --verify "${HASHTREEFILENAME}".asc
+# check file tree signature
+gpg --verify "${TREEFILENAME}".asc
+
+set +e
+
+find . -type f | \
+  fgrep -v "${TREEFILENAME}" | \
+  fgrep -v "${TREEFILENAME}".asc | \
+  fgrep -v "${HASHTREEFILENAME}" | \
+  fgrep -v "${HASHTREEFILENAME}".asc | \
+  sort | \
+  diff --suppress-common-lines -u \
+    --label 'previous tree' "${TREEFILENAME}" \
+    --label 'current tree' -
+
+# check hashes
+echo checking hashes...
+sha512sum -c --quiet "${HASHTREEFILENAME}"
